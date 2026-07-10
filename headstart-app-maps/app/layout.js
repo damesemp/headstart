aa7d07c52e867e3f6b60706961b18bb2016646bb
damesemp <damian.semple@astutegroup.com>
2026-07-10 09:11:09 +0100

    Change body background color to light gray

--- headstart-app-maps/app/layout.js
+++ headstart-app-maps/app/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, background: "#0e0e0e" }}>{children}</body>
+      <body style={{ margin: 0, background: "#f0f0f0" }}>{children}</body>
     </html>
   );
 }
